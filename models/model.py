d40590eb35199e95dd0b703d5e947e96153b8ff4
ejrtks1020 <49896157+ejrtks1020@users.noreply.github.com>
2023-06-10 05:15:45 +0900

    feat: add models

--- models/model.py
+++ models/model.py
@@ -19,7 +19,54 @@ class fcn_resnet50(nn.Module):
         outputs = self.forward(image)
         return outputs
 
+class fcn_resnet101(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = models.segmentation.fcn_resnet101(pretrained=True)
+        self.model.classifier[4] = nn.Conv2d(512, num_classes, kernel_size=1)
 
+    def forward(self, image):
+        return self.model(image)["out"]
+
+    def train_step(self, image):
+        image = image.to(self.device)
+
+        outputs = self.forward(image)
+        return outputs
+    
+class deeplabv3_resnet50(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = models.segmentation.deeplabv3_resnet50(pretrained=True)
+        self.model.classifier[4] = nn.Conv2d(256, num_classes, kernel_size=1)
+
+    def forward(self, image):
+        return self.model(image)["out"]
+
+    def train_step(self, image):
+        image = image.to(self.device)
+
+        outputs = self.forward(image)
+        return outputs        
+
+class deeplabv3_resnet101(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = models.segmentation.deeplabv3_resnet101(pretrained=True)
+        self.model.classifier[4] = nn.Conv2d(256, num_classes, kernel_size=1)
+
+    def forward(self, image):
+        return self.model(image)["out"]
+
+    def train_step(self, image):
+        image = image.to(self.device)
+
+        outputs = self.forward(image)
+        return outputs
+    
 class fcn32(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
