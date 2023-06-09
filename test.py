e2dc2749640f28c3c4fa9a0f3b729ec8f0daa1c4
lijm1358 <lijm1358@gmail.com>
2023-06-09 12:55:52 +0000

    chore: add test file for github actions

---
+++ test.py
@@ -0,0 +1,11 @@
+import random
+import torch
+from smp.utils.util import *
+import abc
+from torch.utils.data import DataLoader
+
+def main():
+    train_loader = DataLoader(dataset=None, batch_size=1,shuffle=True,num_workers=0,drop_last=True,)
+
+if __name__ == '__main__':
+    main()
