5b41bf6b9229d9e7bee21d490950124cfbc5997a
KimGeunUk <kgw5430@naver.com>
2023-06-09 01:11:07 +0000

    feat: amp, wandb, ealry stop

--- smp/runner/train_runner.py
+++ smp/runner/train_runner.py
@@ -3,6 +3,7 @@ import os
 import datetime
 
 # external library
+import wandb
 from tqdm.auto import tqdm
 
 # torch
@@ -17,24 +18,56 @@ def train(args, model, data_loader, val_loader, criterion, optimizer):
     print(f'Start training..')
     set_seed(args.seed)
     n_class = len(CLASSES)
+    
+    # Early Stop
     best_dice = 0.
+    best_loss = float("inf")
+    patience_limit = args.patience_limit
+    patience = 0
     
     for epoch in range(args.epochs):
         model.train()
 
-        for step, (images, masks) in enumerate(data_loader):            
-            # gpu 연산을 위해 device 할당
-            images, masks = images.cuda(), masks.cuda()
-            model = model.cuda()
-            
-            # inference
-            outputs = model(images)
-            
-            # loss 계산
-            loss = criterion(outputs, masks)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        # Creates a GradScaler once at the beginning of training.
+        if args.f16: scaler = torch.cuda.amp.GradScaler()
+
+        for step, (images, masks) in enumerate(data_loader):
+            if args.f16:
+                # Casts operations to mixed precision
+                with torch.cuda.amp.autocast():
+                    # gpu 연산을 위해 device 할당
+                    images, masks = images.cuda(), masks.cuda()
+                    model = model.cuda()
+                    
+                    # inference
+                    outputs = model(images)
+                    
+                    # loss 계산
+                    loss = criterion(outputs, masks)
+                
+                # Scales the loss, and calls backward() 
+                # to create scaled gradients 
+                scaler.scale(loss).backward()
+
+                # Unscales gradients and calls 
+                # or skips optimizer.step() 
+                scaler.step(optimizer)
+
+                # Updates the scale for next iteration 
+                scaler.update()
+            else:
+                # gpu 연산을 위해 device 할당
+                images, masks = images.cuda(), masks.cuda()
+                model = model.cuda()
+                
+                # inference
+                outputs = model(images)
+                
+                # loss 계산
+                loss = criterion(outputs, masks)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
             
             # step 주기에 따른 loss 출력
             if (step + 1) % args.log_step == 0:
@@ -42,19 +75,26 @@ def train(args, model, data_loader, val_loader, criterion, optimizer):
                     f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} | '
                     f'Epoch [{epoch+1}/{args.epochs}], '
                     f'Step [{step+1}/{len(data_loader)}], '
-                    f'Loss: {round(loss.item(),4)}'
+                    f'Loss: {round(loss.item(),6)}'
                 )
-             
+        
+            wandb.log({'train/loss': loss.item()})
         # validation 주기에 따른 loss 출력 및 best model 저장
         if (epoch + 1) % args.val_every == 0:
             dice = valid(args, epoch + 1, model, val_loader, criterion)
-            
-            if best_dice < dice:
-                print(f"Best performance at epoch: {epoch + 1}, {best_dice:.4f} -> {dice:.4f}")
+
+            if best_dice <= dice:
+                print(f"Best performance at epoch: {epoch + 1}, {best_dice:.6f} -> {dice:.6f}")
                 print(f"Save model in {args.save_model_dir}")
                 best_dice = dice
+                patience = 0                
                 torch.save(model, os.path.join(args.save_model_dir, args.save_model_fname))
-                
+            else:
+                patience += 1
+                if patience >= patience_limit: break
+                            
+    wandb.log({'epoch': epoch})
+      
 def valid(args, epoch, model, data_loader, criterion, thr=0.5):
     print(f'Start validation #{epoch:2d}')
     set_seed(args.seed)
@@ -89,16 +129,20 @@ def valid(args, epoch, model, data_loader, criterion, thr=0.5):
             
             dice = dice_coef(outputs, masks)
             dices.append(dice)
+            
+            wandb.log({'valid/loss': loss.item()})
                 
     dices = torch.cat(dices, 0)
     dices_per_class = torch.mean(dices, 0)
     dice_str = [
-        f"{c:<12}: {d.item():.4f}"
+        f"{c:<12}: {d.item():.6f}"
         for c, d in zip(CLASSES, dices_per_class)
     ]
     dice_str = "\n".join(dice_str)
     print(dice_str)
     
     avg_dice = torch.mean(dices_per_class).item()
+    
+    wandb.log({'valid/dice': avg_dice})
     
     return avg_dice
