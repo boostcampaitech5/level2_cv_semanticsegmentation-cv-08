a1a37d7ed01c57a72b8bd63fb607e8d781beb08f
jshye <50259233+jshye@users.noreply.github.com>
2023-06-12 05:10:42 +0000

    feat: add StratifiedGroupKFold, save split train/valid file names to json

--- make_pickle_dataset.py
+++ make_pickle_dataset.py
@@ -6,17 +6,18 @@ import pickle
 from tqdm import tqdm
 
 import augmentations
-from datasets import XRayDataset
+from datasets import XRayDataset, XRayDatasetV2
 from utils import read_json
 
 
-def main(config, target):
-    if not os.path.exists(target):
-        os.makedirs(os.path.join(target, "train"))
-        os.makedirs(os.path.join(target, "valid"))
+def main(config, args):
+    if not os.path.exists(args.target):
+        os.makedirs(os.path.join(args.target, "train"))
+        os.makedirs(os.path.join(args.target, "valid"))
 
-    train_path = os.path.join(target, "train")
-    valid_path = os.path.join(target, "valid")
+    train_path = os.path.join(args.target, "train")
+    valid_path = os.path.join(args.target, "valid")
+    
     if config.augmentations:
         train_aug = getattr(augmentations, config.augmentations.name)(
             **config.augmentations.parameters
@@ -28,14 +29,17 @@ def main(config, target):
         train_aug = None
         valid_aug = None
 
-    train_dataset = XRayDataset(config, transforms=train_aug)
+    if args.v2:
+        train_dataset = XRayDatasetV2(config, transforms=train_aug)
+        valid_dataset = XRayDatasetV2(config, is_train=False, transforms=valid_aug)
+    else:
+        train_dataset = XRayDataset(config, transforms=train_aug)
+        valid_dataset = XRayDataset(config, is_train=False, transforms=valid_aug)
 
     for i in tqdm(range(len(train_dataset))):
         g = train_dataset[i]
         with gzip.open(os.path.join(train_path, f"{i}.pkl"), mode="wb") as f:
             pickle.dump(g, f)
-
-    valid_dataset = XRayDataset(config, is_train=False, transforms=valid_aug)
 
     for i in tqdm(range(len(valid_dataset))):
         g = valid_dataset[i]
@@ -55,6 +59,9 @@ if __name__ == "__main__":
         type=str,
         help="cache dataset root directory",
     )
+    parser.add_argument('--v2', action='store_true', help='Use XRayDatasetV2')
+
     args = parser.parse_args()
     config = read_json(args.config)
-    main(config, args.target)
+    
+    main(config, args)
